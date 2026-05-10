e1c370bd8fca807057ae0c89044891139bf442fa
Stefan Reinauer <stefan.reinauer@coreboot.org>
2026-05-10 09:23:39 -0700

    Fix Unix SDL frame pumping and interrupts
    Fix Unix SDL frame pumping and interrupts in the affected Unix and macOS
    path.
    
    This is needed so the native Unix backend can provide the same runtime
    behavior without depending on Windows-only host code.

--- od-unix/support.cpp
+++ od-unix/support.cpp
@@ -7,13 +7,74 @@
 
 #include "options.h"
 #include "uae.h"
+#include "memory.h"
+#include "newcpu.h"
+#include "host.h"
+
+#include <signal.h>
+#include <string.h>
 
 uae_u8 *natmem_offset;
 uae_u8 *natmem_reserved;
 uae_u32 natmem_reserved_size;
 
+static volatile sig_atomic_t unix_signal_quit_requested;
+static volatile sig_atomic_t unix_exit_signal;
+static bool unix_signal_quit_dispatched;
+static bool unix_signals_installed;
+
+static void unix_alarm_handler(int)
+{
+    int sig = unix_exit_signal ? unix_exit_signal : SIGTERM;
+    _exit(128 + sig);
+}
+
+static void unix_signal_handler(int sig)
+{
+    unix_signal_quit_requested = 1;
+    unix_exit_signal = sig;
+    quit_program = -UAE_QUIT;
+    set_special_exter(SPCFLAG_BRK | SPCFLAG_MODE_CHANGE);
+    alarm(2);
+}
+
+bool unix_host_quit_requested(void)
+{
+    return unix_signal_quit_requested != 0;
+}
+
+void unix_host_check_quit(void)
+{
+    if (unix_signal_quit_requested && !unix_signal_quit_dispatched) {
+        unix_signal_quit_dispatched = true;
+        uae_quit();
+        set_special(SPCFLAG_BRK | SPCFLAG_MODE_CHANGE);
+    }
+}
+
+void setup_brkhandler(void)
+{
+    if (unix_signals_installed) {
+        return;
+    }
+
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sigemptyset(&sa.sa_mask);
+    sa.sa_handler = unix_signal_handler;
+
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+    sigaction(SIGHUP, &sa, NULL);
+
+    sa.sa_handler = unix_alarm_handler;
+    sigaction(SIGALRM, &sa, NULL);
+    unix_signals_installed = true;
+}
+
 int machdep_init(void)
 {
+    setup_brkhandler();
     return 1;
 }
 
@@ -22,9 +83,19 @@ int sleep_resolution;
 int sleep_millis(int ms)
 {
     if (ms <= 0) {
+        unix_host_check_quit();
         return 0;
     }
-    usleep((useconds_t)ms * 1000);
+    struct timespec ts;
+    ts.tv_sec = ms / 1000;
+    ts.tv_nsec = (ms % 1000) * 1000000;
+    while (nanosleep(&ts, &ts) != 0 && errno == EINTR) {
+        unix_host_check_quit();
+        if (unix_host_quit_requested()) {
+            break;
+        }
+    }
+    unix_host_check_quit();
     return 0;
 }
 
@@ -45,13 +116,19 @@ void sleep_cpu_wakeup(void)
 int target_sleep_nanos(int ns)
 {
     if (ns <= 0) {
+        unix_host_check_quit();
         return 0;
     }
     struct timespec ts;
     ts.tv_sec = ns / 1000000000;
     ts.tv_nsec = ns % 1000000000;
     while (nanosleep(&ts, &ts) != 0 && errno == EINTR) {
+        unix_host_check_quit();
+        if (unix_host_quit_requested()) {
+            break;
+        }
     }
+    unix_host_check_quit();
     return 0;
 }
 
