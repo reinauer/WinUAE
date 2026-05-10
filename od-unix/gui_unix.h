e2aa63ca68e78ddb883c99a10201e07f75bc7195
Stefan Reinauer <stefan.reinauer@coreboot.org>
2026-05-10 16:58:32 -0700

    od-unix: run Qt launcher from GUI startup
    Connect the Unix GUI startup hook to the integrated Qt launcher bridge
    so the configuration dialog can run before emulation starts.

--- od-unix/gui_unix.h
+++ od-unix/gui_unix.h
@@ -1,11 +1,3 @@
 #pragma once
 
-#include "sysdeps.h"
-
-enum {
-    UNIX_GUI_EARLY_NONE = 0,
-    UNIX_GUI_EARLY_EXIT = 1,
-    UNIX_GUI_EARLY_START = 2
-};
-
-int unix_gui_handle_early_options(int argc, TCHAR **argv, int *exit_code);
+void unix_gui_set_main_args(int argc, TCHAR **argv);
--- od-unix/qt/launcher_bridge.h
+++ od-unix/qt/launcher_bridge.h
@@ -2,7 +2,8 @@
 
 enum {
     WINUAE_QT_LAUNCHER_EXIT = 1,
-    WINUAE_QT_LAUNCHER_START = 2
+    WINUAE_QT_LAUNCHER_START = 2,
+    WINUAE_QT_LAUNCHER_ERROR = 3
 };
 
 int runWinUaeQtLauncherForStartupConfig(int argc, char **argv, int *exit_code);
