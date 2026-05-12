03021c698a5db6ae0bedb11cf4bd005e17117ea4
Stefan Reinauer <stefan.reinauer@coreboot.org>
2026-05-11 17:15:13 -0700

    Handle Unix Qt runtime media shortcuts
    Handle Unix Qt runtime media shortcuts in the affected Unix and macOS
    path.
    
    This brings the Unix Qt frontend closer to the Windows GUI by exposing
    supported controls and keeping unsupported ones honest.

--- od-unix/qt/launcher_bridge.cpp
+++ od-unix/qt/launcher_bridge.cpp
@@ -7,6 +7,7 @@
 #include <QString>
 
 #include <stdio.h>
+#include <string.h>
 
 int runWinUaeQtLauncherForPrefs(int argc, char **argv, struct uae_prefs *prefs, int *exitCode)
 {
@@ -41,4 +42,40 @@ int runWinUaeQtLauncherForPrefsWithConfig(int argc, char **argv, struct uae_pref
         *exitCode = 0;
     }
     return WINUAE_QT_LAUNCHER_EXIT;
+}
+
+int runWinUaeQtRuntimeFileDialog(int argc, char **argv, int shortcut, const char *initialPath, char *selectedPath, size_t selectedPathLen, int *exitCode)
+{
+    if (!selectedPath || selectedPathLen == 0) {
+        if (exitCode) {
+            *exitCode = 1;
+        }
+        return WINUAE_QT_LAUNCHER_ERROR;
+    }
+    selectedPath[0] = 0;
+
+    const QString initial = initialPath && initialPath[0]
+        ? QString::fromLocal8Bit(initialPath)
+        : QString();
+    const WinUaeQtRuntimeFileDialogResult result = runWinUaeQtRuntimeFileDialog(argc, argv, shortcut, initial);
+    if (!result.accepted) {
+        if (exitCode) {
+            *exitCode = 0;
+        }
+        return WINUAE_QT_LAUNCHER_EXIT;
+    }
+
+    const QByteArray path = result.path.toLocal8Bit();
+    if (size_t(path.size()) >= selectedPathLen) {
+        fprintf(stderr, "Unix Qt UI failed: selected path is too long\n");
+        if (exitCode) {
+            *exitCode = 1;
+        }
+        return WINUAE_QT_LAUNCHER_ERROR;
+    }
+    memcpy(selectedPath, path.constData(), size_t(path.size()) + 1);
+    if (exitCode) {
+        *exitCode = 0;
+    }
+    return WINUAE_QT_LAUNCHER_START;
 }
