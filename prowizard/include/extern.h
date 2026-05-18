38133ece51b0496d3484370e661f9567cdf4d0fb
Stefan Reinauer <stefan.reinauer@coreboot.org>
2026-05-17 18:26:22 -0700

    prowizard: expose host save prompt callbacks
    Add the small shared Pro Wizard hooks needed by non-Windows frontends
    to ask the user before saving ripped modules.

--- prowizard/include/extern.h
+++ prowizard/include/extern.h
@@ -332,7 +332,9 @@ extern short test_smps ( long, long, long, Uchar, Uchar );
 extern long  PWGetFileSize ( char * );
 extern FILE * PW_fopen ( char *, char * );
 extern void fillPTKtable ( Uchar[37][2] );
+#ifndef htonl
 extern unsigned int htonl(unsigned int);
+#endif
 
 /* globals */
 /* Some say it's badly coding when using Globals ... sure it is, now what's the solution ? */
@@ -357,4 +359,3 @@ extern Uchar Amiga_EXE_Header;
 extern void pw_write_log (const char *, ...);
 extern FILE *moduleripper2_fopen (const char *name, const char *mode, const char *aid, int addr, int size);
 extern FILE *moduleripper_fopen (const char *aname, const char *amode);
-
