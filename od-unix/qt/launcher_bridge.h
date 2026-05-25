a016c66d736fa28b312a1430b95f917821ed9b15
Stefan Reinauer <stefan.reinauer@coreboot.org>
2026-05-25 10:28:14 -0700

    qt: use core board tables in unix launcher
    Populate the Unix Qt accelerator and expansion board lists from the
    shared cpuboards[] and expansionroms[] tables instead of private UI
    catalogs.

--- od-unix/qt/launcher_bridge.h
+++ od-unix/qt/launcher_bridge.h
@@ -12,5 +12,6 @@ enum {
 
 int runWinUaeQtLauncherForPrefs(int argc, char **argv, struct uae_prefs *prefs, int *exit_code);
 int runWinUaeQtLauncherForPrefsWithConfig(int argc, char **argv, struct uae_prefs *prefs, const char *initial_config_path, int *exit_code);
+int runWinUaeQtBoardCatalogDump(void);
 int runWinUaeQtRuntimeFileDialog(int argc, char **argv, int shortcut, const char *initial_path, char *selected_path, size_t selected_path_len, int *exit_code);
 int runWinUaeQtMessageBox(int argc, char **argv, int flags, const char *message, int *exit_code);
