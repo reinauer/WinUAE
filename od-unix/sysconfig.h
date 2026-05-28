fed4e8f52a8cf924242602f23b3547901fcf292c
Stefan Reinauer <stefan.reinauer@coreboot.org>
2026-05-28 00:15:41 -0700

    od-unix: add package build compatibility defines
    Add the Unix compatibility definitions needed by the packaged build
    after the shared portability fixes are in place.

--- od-unix/sysconfig.h
+++ od-unix/sysconfig.h
@@ -156,8 +156,8 @@ typedef int16_t INT16;
 typedef uint16_t UINT16;
 typedef int32_t INT32;
 typedef uint32_t UINT32;
-typedef int64_t INT64;
-typedef uint64_t UINT64;
+typedef signed long long INT64;
+typedef unsigned long long UINT64;
 #endif
 
 #define BYTE int8_t
