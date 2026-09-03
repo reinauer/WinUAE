61a3d6a324edae78dd873c969c2dfdf9902ae929
Stefan Reinauer <stefan.reinauer@coreboot.org>
2026-09-03 11:55:57 -0700

    qt: stub FloppyBridge helpers when support is disabled
    Keep the launcher linkable when optional FloppyBridge support is
    disabled by providing inline no-op Qt helpers. This preserves the
    existing fallback behavior without compiling the plugin-backed
    implementation.

--- od-unix/qt/floppybridge_ui.h
+++ od-unix/qt/floppybridge_ui.h
@@ -10,7 +10,30 @@ struct WinUaeQtFloppyBridgeProfile {
     QString name;
 };
 
+#if defined(UAE_UNIX_WITH_FLOPPYBRIDGE) \
+    && UAE_UNIX_WITH_FLOPPYBRIDGE
 bool winuaeQtFloppyBridgeAvailable();
 QVector<WinUaeQtFloppyBridgeProfile> winuaeQtFloppyBridgeProfiles();
 bool winuaeQtManageFloppyBridgeProfiles(QWidget *parent);
 QString winuaeQtFloppyBridgeInformation();
+#else
+inline bool winuaeQtFloppyBridgeAvailable()
+{
+    return false;
+}
+
+inline QVector<WinUaeQtFloppyBridgeProfile> winuaeQtFloppyBridgeProfiles()
+{
+    return {};
+}
+
+inline bool winuaeQtManageFloppyBridgeProfiles(QWidget *)
+{
+    return false;
+}
+
+inline QString winuaeQtFloppyBridgeInformation()
+{
+    return {};
+}
+#endif
